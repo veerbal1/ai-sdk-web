f8cf5e48cd62a1d3b4cb75f1a0dec934db1c1585
veerbal1 <veerbalsingh1@gmail.com>
2025-04-04 17:53:08 +0530

    Refactor NavMain component to highlight active navigation items using usePathname hook for improved user experience and visual feedback.

--- shared/components/nav-main.tsx
+++ shared/components/nav-main.tsx
@@ -6,6 +6,7 @@ import {
   type Icon 
 } from "@tabler/icons-react"
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import { Button } from "@/shared/components/ui/button"
 import {
@@ -25,22 +26,31 @@ export function NavMain({
     icon?: Icon
   }[]
 }) {
+  const pathname = usePathname();
+
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu>
-          {items.map((item) => (
-            <SidebarMenuItem key={item.title}>
-              <Link href={item.url} passHref legacyBehavior>
-                <SidebarMenuButton tooltip={item.title} asChild>
-                  <a>
-                    {item.icon && <item.icon />}
-                    <span>{item.title}</span>
-                  </a>
-                </SidebarMenuButton>
-              </Link>
-            </SidebarMenuItem>
-          ))}
+          {items.map((item) => {
+            const isActive = pathname === item.url;
+            return (
+              <SidebarMenuItem key={item.title}>
+                <Link href={item.url} passHref legacyBehavior>
+                  <SidebarMenuButton 
+                    tooltip={item.title} 
+                    asChild 
+                    data-active={isActive}
+                  >
+                    <a>
+                      {item.icon && <item.icon />}
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </Link>
+              </SidebarMenuItem>
+            )
+          })}
         </SidebarMenu>
       </SidebarGroupContent>
     </SidebarGroup>
