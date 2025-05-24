8d1d08598d8853f6cda1d8da1bb3a0a26b39512b
veerbal1 <veerbalsingh1@gmail.com>
2025-05-24 11:13:15 +0530

    Added text labels

--- app/(pages)/reasoning-animation/page.tsx
+++ app/(pages)/reasoning-animation/page.tsx
@@ -2,7 +2,14 @@ import ReasoningAnimation from "@/features/reasoning-animation";
 
 const ReasoningAnimationPage = () => {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full relative">
+      <div className="flex flex-col gap-2 absolute top-0 left-0 bg-white p-4">
+        <h1 className="text-2xl font-bold">Reasoning Animation Simulator</h1>
+        <p className="text-sm text-gray-500">
+          This is a page that shows a reasoning animation. It is a animation
+          simulation of the reasoning process of a LLM.
+        </p>
+      </div>
       <ReasoningAnimation />
     </div>
   );
